5a35f3e8742b5aa8c7cbcfbb192a90bd120dfa7a
Arthur <Arth.valadao@gmail.com>
2022-10-20 00:21:05 -0400

    insert axios and gitignore

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
 
-//Aqui é o Server
 app.listen('3000')
 
-
-//exemplo POST com retorno do conteudo após o POST com status 200
-app.route('/').post( (req, res) => res.send(req.body))
-
-//middleware
-app.use(express.json())
-
-let author= "Arthur"
-
-//Método PUT alterando um campo
-app.route('/').put((req, res) => { 
-  author = req.body.author
-  res.send(author)
+app.route('/').get( (req, res) => { 
+  axios.get('https://api.github.com/users/Arthur-Valadao')
+  .then(result => res.send(result.data))
+  .catch(error => console.error(error))
 })
-
-app.route('/').get( (req, res) => res.send(author) )
-
 
