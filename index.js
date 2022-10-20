079d4c090f5a8adb7e8042871fe9ca48da9b6a0a
Arthur <Arth.valadao@gmail.com>
2022-10-19 23:22:22 -0400

    insert exemples of HTTP Methods

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 const express = require('express')
 const app = express()
 
+//Aqui é o Server
 app.listen('3000')
+
+
+//exemplo POST com retorno do conteudo após o POST com status 200
+app.route('/').post( (req, res) => res.send(req.body))
+
+//middleware
+app.use(express.json())
+
+let author= "Arthur"
+
+//Método PUT alterando um campo
+app.route('/').put((req, res) => { 
+  author = req.body.author
+  res.send(author)
+})
+
+app.route('/').get( (req, res) => res.send(author) )
+
+
